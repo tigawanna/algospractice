0c5e8b030f5f93762cb0c8d17b19e3e2f131341e
tigawanna <denniskinuthiaw@gmail.com>
2022-08-22 15:41:06 +0300

    simple binary search

--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct TreeNode{
 int value;
@@ -52,32 +53,49 @@ void printTree(TreeNode *root){
     printTree_rec(root, 0);
 }
 
+
+bool insertnumber(TreeNode **rootptr, int value){
+TreeNode *root = *rootptr;
+if(root == NULL){
+    (*rootptr) = createnode(value);
+    return true;
+}
+
+if(value == root->value){
+   return false;
+}
+if(value<root->value){
+   return insertnumber(&(root->left),value);
+}
+else{
+   return insertnumber(&(root->right), value);
+}
+}
+
+
+bool findNumber(TreeNode *root,int value){
+if (root == NULL) return false;
+if (root->value == value) return true;
+
+if(value<root->value){
+    return findNumber(root->left,value);
+}else{
+    return findNumber(root->right, value);
+}
+}
+
 int main(int argc, char const *argv[])
 {
- TreeNode *n1 = createnode(1);
- TreeNode *n2 = createnode(2);
- TreeNode *n3 = createnode(3);
- TreeNode *n4 = createnode(4);
- TreeNode *n5 = createnode(5);
- TreeNode *n6 = createnode(6);
-
-
-n1->left=n2;
-n1->right=n3;
-n3->left=n4;
-n3->right=n5;
-
-printTree(n1);
-
-
-
-
- free(n1);
- free(n2);
- free(n3);
- free(n4);
- free(n5);
- free(n6);
- return 0;
+TreeNode *root= NULL;
+insertnumber(&root, 15);
+    insertnumber(&root, 12);
+        insertnumber(&root, 1);
+            insertnumber(&root, 3);
+                insertnumber(&root, 30);
+                    insertnumber(&root, 165);
+printTree(root);
+printf("boolean return afetr search for the item\n");
+printf("%d",findNumber(root,1578));
+                        return 0;
 }
 
